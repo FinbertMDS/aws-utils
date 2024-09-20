1ac31aea9276a3ba68a55ff32d98d5637c3fb68f
ngohuyp <ngohuyp@systena.co.jp>
2024-09-20 15:19:13 +0900

    add nginx conf for sudoku, trello update scripts

--- commands.sh
+++ commands.sh
@@ -1,13 +1,13 @@
 # docker compose
-dc_latest_version=`curl -s https://api.github.com/repos/docker/compose/releases/latest | \
+export dc_latest_version=`curl -s https://api.github.com/repos/docker/compose/releases/latest | \
         grep tag_name | \
         sed -e 's/[^0-9\.]//g'`
-sudo curl -L "https://github.com/docker/compose/releases/download/$dc_latest_version/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/v$dc_latest_version/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
 # certbot ssl
-sudo certbot certonly --standalone --email ngovanhuy.cntt3@gmail.com -d finbertngo.com -d walica-be.finbertngo.com -d walica-fe.finbertngo.com
+sudo certbot certonly --standalone --email ngovanhuy.cntt3@gmail.com -d finbertngo.com -d trello.finbertngo.com
 
 # pnpm install
 curl -fsSL https://get.pnpm.io/install.sh | sh -
--- user-data.sh
+++ user-data.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash -x
-default_user='ec2-user'
+export default_user='ec2-user'
 
 # programming languages
-python_version='3.12.6'
-node_version='20.17.0'
+export python_version='3.12.6'
+export node_version='20.17.0'
 
 # installation: docker
 sudo dnf install -y docker
@@ -12,22 +12,17 @@ sudo usermod -aG docker ${default_user}
 sudo -u ${default_user} newgrp docker
 sudo systemctl start docker
 
+# docker compose
+export dc_latest_version=`curl -s https://api.github.com/repos/docker/compose/releases/latest | \
+        grep tag_name | \
+        sed -e 's/[^0-9\.]//g'`
+sudo curl -L "https://github.com/docker/compose/releases/download/v$dc_latest_version/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
 # installation: build essentials
 sudo dnf install -y gcc git
 sudo dnf install -y bzip2-devel libffi-devel libyaml-devel openssl-devel readline-devel sqlite-devel xz-devel zlib-devel
-
-# installation: pyenv
-curl https://pyenv.run | bash
-TMPDIR="${PWD}/tmp" pyenv install ${python_version}
-pyenv global ${python_version}
-
-# installation: nodenv
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
-source ~/.bashrc
-
-
-# installation: golang
-sudo dnf install golang -y -q
 
 # installation: certbot
 sudo dnf install -y python3 augeas-libs cronie
@@ -39,3 +34,16 @@ sudo ln -s /opt/certbot/bin/certbot /usr/bin/certbot
 # installation: nginx
 sudo dnf install -y nginx
 
+# installation: nvm node
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+source ~/.bashrc
+nvm install --lts
+npm install -g yarn
+
+# installation: pyenv
+curl https://pyenv.run | bash
+export TMPDIR="${PWD}/tmp" pyenv install ${python_version}
+pyenv global ${python_version}
+
+# installation: golang
+sudo dnf install golang -y -q
